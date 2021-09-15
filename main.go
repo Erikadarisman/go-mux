34eae0f2db4de68c4ba50cc49f171dfe5eea13c2
Eri Kadarisman <erikadarisman@gmail.com>
2021-09-15 18:28:26 +0700

    deleted some code

--- main.go
+++ main.go
@@ -1,31 +1,3 @@
-// //file main.go
-// package main
-
-// import (
-// 	"fmt"
-// 	"net/http"
-
-// 	"github.com/gorilla/mux"
-// )
-
-// func handlerFunc(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "text/html")
-// 	if r.URL.Path == "/" {
-// 		fmt.Fprint(w, "<h1>Selamat datang di skillplus</h1>")
-// 	} else if r.URL.Path == "/aboutus" {
-// 		fmt.Fprint(w, "<h1>Skillplus adalah web tutorial seputar informasi teknologi</h1>")
-// 	} else {
-// 		w.WriteHeader(http.StatusNotFound)
-// 		fmt.Fprint(w, "<h1>Halaman yang dicari tidak ditemukan</h1>")
-// 	}
-// }
-// func main() {
-// 	r := mux.NewRouter()
-// 	r.HandleFunc("/", handlerFunc)
-// 	r.HandleFunc("/aboutus", handlerFunc)
-// 	http.ListenAndServe(":3000", r)
-// }
-
 package main
 
 import (
@@ -35,16 +7,6 @@ import (
 	"github.com/Erikadarisman/go-mux/routes"
 	"github.com/gorilla/mux"
 )
-
-type Response struct {
-	Persons []Person `json:"persons"`
-}
-
-type Person struct {
-	Id        int    `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-}
 
 func main() {
 	log.Println("starting API server")
